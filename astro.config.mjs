b20f0c0421a313ee28ff51bc09af01adf320844a
Chen Hui Jing <1461498+huijing@users.noreply.github.com>
2025-05-23 17:23:06 +0000

    Add replit specific configuration updates

--- astro.config.mjs
+++ astro.config.mjs
@@ -9,4 +9,9 @@ export default defineConfig({
   adapter: node({
     mode: "standalone",
   }),
+  vite: {
+    server: {
+      allowedHosts: ["f44e723f-799e-4389-b790-497c0703014e-00-2qdf2j0ik84y2.picard.replit.dev", 'localhost']
+    }
+  }
 });
